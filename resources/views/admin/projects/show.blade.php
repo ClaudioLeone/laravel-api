2ff6dad2f399424f59997825f8ad7653860011cd
ClaudioLeone <claleone92@gmail.com>
2023-06-10 00:52:02 +0200

    Implemented project deletion in 'show' page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -11,8 +11,14 @@
             <li class="list-group-item text-white bg-dark"><strong>SLUG: </strong>{{ $project->slug }}</li>
         </ul>
         <div class="card-body">
-            <a href="{{ route('admin.projects.index') }}" class="card-link"><i class="fa-solid fa-square-caret-left"></i> Torna alla lista</a>
-            <a href="#" class="card-link text-danger"><i class="fa-solid fa-trash-can"></i> Cancella</a>
+            <a href="{{ route('admin.projects.index') }}" class="btn ms-confirm-btn"><i class="fa-solid fa-square-caret-left"></i> Torna alla lista</a>
+            {{-- <a href="#" class="btn ms-undo-btn"><i class="fa-solid fa-trash-can"></i> Cancella</a> --}}
+
+            <form class="d-inline-block" action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="btn ms-undo-btn" href="{{ route('admin.projects.destroy', $project->slug) }}"><i class="fa-regular fa-trash-can"></i> Cancella</button>
+            </form>
         </div>
     </div>
 @endsection
