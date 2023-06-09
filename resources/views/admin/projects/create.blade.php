ba37f9c6ef910de4288fdb8bae63c3e49be3eda6
ClaudioLeone <claleone92@gmail.com>
2023-06-09 18:16:23 +0200

    Implemented 'create' and 'store' CRUD functions

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -7,11 +7,11 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title">
+            <input type="text" class="form-control" id="title" name="title">
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Descrizione</label>
-            <textarea class="form-control" id="content" rows="3"></textarea>
+            <textarea class="form-control" id="content" rows="3" name="content"></textarea>
         </div>
         <button class="btn btn-success" type="submit">Crea</button>
     </form>
