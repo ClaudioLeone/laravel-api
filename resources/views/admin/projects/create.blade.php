92767aa8d64a7b198a14e6c170dac3ce81602a79
ClaudioLeone <claleone92@gmail.com>
2023-06-15 23:34:36 +0200

    Added controls for technology checkboxes

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -39,7 +39,7 @@
                 <div class="d-flex flex-wrap">
                     @foreach ($technologies as $technology)
                         <div class="form-check mt-2 me-4">
-                            <input class="form-check-input" type="checkbox" name="technologies[]" value="{{ $technology->id }}" id="tech-{{ $technology->id }}">
+                            <input class="form-check-input" type="checkbox" name="technologies[]" value="{{ $technology->id }}" id="tech-{{ $technology->id }}" @checked(in_array($technology->id, old('technologies', [])))>
                             <label class="form-check-label" for="tech-{{ $technology->id }}">
                                 {{ $technology->name }}
                             </label>
